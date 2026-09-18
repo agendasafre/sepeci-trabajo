73df935e8760ec07227cced5f315615186170219
petro232 <andpetronio@gmail.com>
2026-09-18 20:00:21 -0300

    updated registration date

--- form-config.js
+++ form-config.js
@@ -1,6 +1,6 @@
 (function exposeFormConfig(globalScope) {
   const formConfig = {
-    startsAt: "2026-09-19T00:00:00-03:00",
+    startsAt: "2026-09-18T00:00:00-03:00",
     expiresAt: "2026-09-28T23:59:59-03:00",
     notStartedMessage:
       "El período de inscripción comienza el 19 de septiembre de 2026",
